23186aa11a73abf59cbadf777ad65c6ee9894add
MuskanJaiswal <muskan.jaiswal8530@gmail.com>
2021-11-21 23:31:08 +0530

    update README.md

--- 3_Implementations/chess.c
+++ 3_Implementations/chess.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 	#include<stdlib.h>
 	#include<conio.h>
-	
+
 	int pwstatus[8] = { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ;
 	int pbstatus[8] = { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 } ;
-	
+
 	char board[8][8] = {
 	                    { 'R' , 'H' , 'C' , 'K' , 'Q' , 'C' , 'H' , 'R' },
 	                    { 'P' , 'P' , 'P' , 'P' , 'P' , 'P' , 'P' , 'P' },
@@ -15,7 +15,7 @@
 	                    { 'p' , 'p' , 'p' , 'p' , 'p' , 'p' , 'p' , 'p' },
 	                    { 'r' , 'h' , 'c' , 'k' , 'q' , 'c' , 'h' , 'r' }
 	                    };
-	
+
 	void display();
 	void change( int , int , int , int ) ;
 	void pawn(int , int ) ;
@@ -29,24 +29,24 @@
 	void player2();
 	int check(int , int ) ;
 	int check2(int , int ) ;
-	
+
 	main()
 	{
 	int  x = 0 ;
 	char ch ;
-	
+
 	    printf( "\n\tWELCOME TO CHESS GAME" ) ;
-	    printf( "\n\n\t By Muskan " ) ;
-	
+	    printf( "\n\n\t By Muskan Jaiswal " ) ;
+
 	    getch();
 	    system( "cls" ) ;
-	
+
 	 do
 	 {
 	 x++ ;
 	 system( "cls" ) ;
 	 display();
-	
+
 	 if( (x%2) == 0 )
 	 {
 	            player2();
@@ -55,157 +55,157 @@
 	 {
 	     player1();
 	 }
-	
+
 	 printf( " \n\nPress Enter To Continue ! \n\n " ) ;
-	
+
 	 ch = getch();
 	 }while( ch == 13 ) ;
-	
+
 	}
-	
+
 	void display()
 	{
 	    int i , j , k ;
-	
+
 	    printf( " " ) ;
 	    for( i=0 ; i<8 ; i++ ) printf( "    %d" , i ) ; printf( "\n" ) ;
-	
+
 	    for( k=0 ; k<8 ; k++ )
 	    {
 	    printf( "  " ) ;
 	    for( i=0 ; i<42 ; i++ ) { printf( "-" ) ; } printf( "\n" ) ; printf( "%d " , k ) ;
-	
+
 	    for( j=0 ; j<8 ; j++ ) { printf( "|| %c " , board[k][j] ) ; }
 	    printf( "|| \n" ) ;
 	    }
-	
+
 	    printf( "  " ) ;
 	    for( i=0 ; i<42 ; i++ ) { printf( "-" ) ; } printf( "\n" ) ;
-	
+
 	}
-	
+
 	void change( int r1 , int c1 , int r2 , int c2 )
 	{
 	    char temp ;
-	
+
 	    temp = board[r1][c1] ;
 	    board[r1][c1] = board[r2][c2] ;
 	    board[r2][c2] = temp ;
-	
+
 	}
-	
+
 	void pawn( int r1 , int c1 ) // paido
 	{
 	    pwstatus[c1]++;
-	
+
 	    printf( "Available are: \n" ) ;
-	
+
 	   if( pwstatus[c1] == 1 )
 	    {
 	        if( board[r1+1][c1] == ' ' )
 	        printf( "%d%d , " , r1+1 , c1 ) ;
-	
+
 	        if( board[r1+2][c1] == ' ' )
 	        printf( "%d%d , " , r1+2 , c1 ) ;
-	
+
 	    }
 	    else
 	    {
 	        if(board[r1+1][c1] == ' ' )
 	        printf( "%d%d , " , r1+1 , c1 ) ;
-	
+
 	        if( check(r1+1 , c1+1) == 1 )
 	        printf( "%d%d* , " , r1+1 , c1+1 ) ;
-	
+
 	        if( check(r1+1 , c1-1) == 1 )
 	        printf( "%d%d* , " , r1+1 , c1-1 ) ;
 	    }
-	
+
 	}
-	
+
 	void rook( int r1 , int c1 )
 	{
 	    int i , j , n ;
-	
+
 	    printf( "Available are: \n" ) ;
-	
+
 	    n=c1;
-	
+
 	    printf( "Horizontally: \n" ) ;
-	
+
 	    while( board[r1][n-1] == ' ' )
 	    {
 	        if( n == 0 ) { break ; }
 	        printf( "%d%d , " , r1 , n-1 ) ;
 	        n-- ;
 	    }
-	
+
 	    n=c1 ;
-	
+
 	    while( board[r1][n+1] == ' '  && (n+1) <= 7 )
 	    {
-	
+
 	        printf( "%d%d , " , r1 , n+1 ) ;
 	        ++n ;
 	    }
-	
+
 	    printf( "\nVertically:\n" ) ;
-	
+
 	    n = r1 ;
-	
+
 	    while( board[n-1][c1] == ' ' && n > -1 )
 	    {
 	        printf( "%d%d , " , n-1 , c1 ) ;
 	        --n ;
 	    }
-	
+
 	    n = r1 ;
-	
+
 	    while( (board[n+1][c1] == ' ') && ( (n) <= 7 ) )
 	    {
 	        printf( "%d%d , " , n+1 , c1 ) ;
 	        ++n ;
 	    }
-	
+
 	}
-	
+
 	void horse( int r1 , int c1 )
 	{
 	    printf( "Available are: " ) ;
-	
-	
+
+
 	    if( board[r1+2][c1+1] == ' ' ) printf( "%d%d, " , r1+2 ,c1+1) ;
 	    if( board[r1+2][c1-1] == ' ' ) { if( (c1-1) > -1 ) printf( "%d%d, " , r1+2, c1-1 ) ; }
-	
+
 	    if( board[r1+1][c1+2] == ' ' ) {  if( (c1+2) != 8 ) printf( "%d%d, " , r1+1 , c1+2 ) ; }
 	    if( board[r1-1][c1+2] == ' ' ) {  printf( "%d%d, " , r1-1 , c1+2 ) ; }
-	
+
 	    if( board[r1-2][c1-1] == ' ' )
 	    {
 	        if( (c1-1) != -1 )
 	        printf( "%d%d, " , r1-2 , c1-1 ) ;
 	    }
-	
+
 	    if( board[r1-2][c1+1] == ' ' ) printf( "%d%d, " , r1-2 , c1+1 ) ;
-	
+
 	    if( board[r1+1][c1-2] == ' ' ) printf( "%d%d, " , r1+1 , c1-2 ) ;
-	
+
 	    if( board[r1-1][c1-2] == ' ' )
 	    {
 	        if( (c1-2) != -1 )
 	            printf( "%d%d, " , r1-1 , c1-2 ) ;
 	    }
 	}
-	
-	
+
+
 	void camel( int r1 , int c1 )
 	{
 	    int a , b , c , d ;
 	    printf( "Available are: \n" ) ;
-	
-	
+
+
 	    a = 1 , b = 1 ;
-	
+
 	    while( board[r1-a][c1+b] == ' ' )
 	    {
 	        if( (r1-a) == -1 || (c1+b) == 8 ) break ;
@@ -213,10 +213,10 @@
 	        a++ ;
 	        b++ ;
 	    }
-	
-	
+
+
 	    a = 1 , b = 1 ;
-	
+
 	    while( board[r1+a][c1-b] == ' ' )
 	    {
 	        if( (r1+a) == 8 || (c1-b) == -1 ) break ;
@@ -224,10 +224,10 @@
 	        a++ ;
 	        b++ ;
 	    }
-	
+
 	    a = 1 , b = 1 ;
-	
-	
+
+
 	    while( board[r1+a][c1+b] == ' ' )
 	    {
 	        if( (r1+a) == 8 || (c1+b) == 8 ) break ;
@@ -235,10 +235,10 @@
 	        a++ ;
 	        b++ ;
 	    }
-	
+
 	    a = 1 ;
 	    b = 1 ;
-	
+
 	    while( board[r1-a][c1-b] == ' ' )
 	    {
 	        if( (r1-a) == -1 || (c1-b) == -1 ) break ;
@@ -246,76 +246,76 @@
 	        a++ ;
 	        b++ ;
 	    }
-	
+
 	}
-	
+
 	void king( int r1 , int c1 )
 	{
 	    printf( "Available are: " ) ;
 	    if( board[r1][c1+1] == ' ' ) printf( "%d%d , " , r1 , c1+1 ) ;
-	
+
 	    if( board[r1][c1-1] == ' ' ) printf( "%d%d , " , r1 , c1-1 ) ;
-	
+
 	    if( board[r1+1][c1] == ' ' ) printf( "%d%d , " , r1+1 , c1 ) ;
-	
+
 	    if( board[r1-1][c1] == ' ' ) printf( "%d%d , " , r1-1 , c1 ) ;
-	
+
 	    if( board[r1+1][c1+1] == ' ' ) printf( "%d%d , " , r1+1 , c1+1 ) ;
-	
+
 	    if( board[r1-1][c1-1] == ' ' ) printf( "%d%d , " , r1-1 , c1-1 ) ;
-	
+
 	    if( board[r1-1][c1+1] == ' ' ) printf( "%d%d , " , r1-1 , c1+1 ) ;
-	
+
 	    if( board[r1+1][c1-1] == ' ' ) printf( "%d%d , " , r1+1 , c1-1 ) ;
 	}
-	
+
 	void queen( int r1 , int c1 )
 	{
 	    int x=1 , y=1 , a , b ;
 	    printf( "Available are: " ) ;
-	
+
 	    printf( "Horizontal: " ) ;
-	
+
 	    while( board[r1][c1-y] == ' ' )
 	    {
 	        if( (c1-y) == -1 ) break ;
 	        printf( "%d%d , " , r1 , c1-y ) ;
 	        y++ ;
 	    }
-	
+
 	    y = 1 ;
-	
+
 	    while( board[r1][c1+y] == ' ' )
 	    {
 	        if( (c1+y) == 8 ) break ;
 	        printf( "%d%d , " , r1 , c1+y ) ;
 	        y++ ;
 	    }
-	
+
 	    printf( "Vertical: " ) ;
-	
+
 	    x = 1 ;
-	
+
 	    while( board[r1-x][c1] == ' ' )
 	    {
 	        if( (r1-x) == -1 ) break ;
 	        printf( "%d%d , " , r1-x , c1 ) ;
 	        x++ ;
 	    }
-	
+
 	    x = 1 ;
-	
+
 	    while( board[r1+x][c1] == ' ' )
 	    {
 	        if( (r1+x) == 8 ) break ;
 	        printf( "%d%d , " , r1+x , c1 ) ;
 	        x++ ;
 	    }
-	
+
 	    printf( "Diagonally: " ) ;
-	
+
 	    a = 1 , b = 1 ;
-	
+
 	    while( board[r1-a][c1+b] == ' ' )
 	    {
 	        if( (r1-a) == -1 || (c1+b) == 8 ) break ;
@@ -323,10 +323,10 @@
 	        a++ ;
 	        b++ ;
 	    }
-	
-	
+
+
 	    a = 1 , b = 1 ;
-	
+
 	    while( board[r1+a][c1-b] == ' ' )
 	    {
 	        if( (r1+a) == 8 || (c1-b) == -1 ) break ;
@@ -334,10 +334,10 @@
 	        a++ ;
 	        b++ ;
 	    }
-	
+
 	    a = 1 , b = 1 ;
-	
-	
+
+
 	    while( board[r1+a][c1+b] == ' ' )
 	    {
 	        if( (r1+a) == 8 || (c1+b) == 8 ) break ;
@@ -345,10 +345,10 @@
 	        a++ ;
 	        b++ ;
 	    }
-	
+
 	    a = 1 ;
 	    b = 1 ;
-	
+
 	    while( board[r1-a][c1-b] == ' ' )
 	    {
 	        if( (r1-a) == -1 || (c1-b) == -1 ) break ;
@@ -356,21 +356,21 @@
 	        a++ ;
 	        b++ ;
 	    }
-	
+
 	}
-	
+
 	void pawnb( int r1 , int c1 ) // paido black
 	{
 	    pbstatus[c1]++;
-	
+
 	    printf( "Available are: \n" ) ;
-	
-	
+
+
 	    if( pbstatus[c1] == 1 )
 	    {
 	        if( board[r1-1][c1] == ' ' )
 	        printf( "%d%d , " , r1-1 , c1 ) ;
-	
+
 	        if( board[r1-2][c1] == ' ' )
 	        printf( "%d%d , " , r1-2 , c1 ) ;
 	    }
@@ -378,28 +378,28 @@
 	    {
 	        if(board[r1-1][c1] == ' ' )
 	        printf( "%d%d , " , r1-1 , c1 ) ;
-	
+
 	        if( check2(r1-1 , c1-1) == 1 )
 	        printf( "%d%d* , " , r1-1 , c1-1 ) ;
-	
+
 	        if( check2(r1-1 , c1+1) == 1 )
 	        printf( "%d%d* , " , r1-1 , c1+1 ) ;
 	    }
-	
+
 	}
-	
+
 	void player1()
 	{
 	    int p1 , p2 , c1 , r1 , c2 , r2;
-	
-	    printf( "\nPLAYER 1 - Big Case\n" ) ;
+
+	    printf( "\nPLAYER 1 - White Big Case\n" ) ;
 	    again1:
-	    printf( "\nEnter Position of Element to change ( RC ): " ) ;
+	    printf( "\nEnter Position of Element to change ( RowColumn ): " ) ;
 	    scanf( "%d" , &p1 ) ;
-	
+
 	    c1 = p1 % 10 ;
 	    r1 = p1 / 10 ;
-	
+
 	    switch( board[r1][c1] )
 	    {
 	        case 'P': pawn( r1 , c1 );
@@ -416,29 +416,29 @@
 	                  break ;
 	        default: printf("Invalid Position ! ") ; goto again1 ;
 	    }
-	
-	    printf( "\nEnter Position of Place to Send ( RC ): " ) ;
+
+	    printf( "\nEnter Position of Place to Send ( RowColumn ): " ) ;
 	    scanf( "%d" , &p2 ) ;
-	
+
 	    c2 = p2 % 10 ;
 	    r2 = p2 / 10  ;
-	
-	
+
+
 	    change(r1,c1,r2,c2) ;
 	}
-	
+
 	void player2()
 	{
 	    int p1 , p2 , c1 , r1 , c2 , r2;
-	
-	    printf( "\nPLAYER 2 - Small Case \n") ;
+
+	    printf( "\nPLAYER 2 - Black Small Case \n") ;
 	    again2:
-	    printf( "\nEnter Position of Element to change ( RC ): " ) ;
+	    printf( "\nEnter Position of Element to change ( RowColumn ): " ) ;
 	    scanf( "%d" , &p1 ) ;
-	
+
 	    c1 = p1 % 10 ;
 	    r1 = p1 / 10 ;
-	
+
 	    switch( board[r1][c1] )
 	    {
 	        case 'p': pawnb( r1 , c1 ) ;
@@ -455,18 +455,18 @@
 	                  break ;
 	        default: printf( "Invalid Position ! " ) ; goto again2 ;
 	    }
-	
-	
-	    printf( "\nEnter Position of Place to Send ( RC ): " ) ;
+
+
+	    printf( "\nEnter Position of Place to Send ( RowColumn ): " ) ;
 	    scanf( "%d" , &p2 ) ;
-	
+
 	    c2 = p2 % 10 ;
 	    r2 = p2 / 10  ;
-	
-	
+
+
 	    change(r1,c1,r2,c2) ;
 	}
-	
+
 	int check(int x , int y )
 	{
 	    switch( board[x][y] )
@@ -480,7 +480,7 @@
 	    default: return 0 ;
 	    }
 	}
-	
+
 	int check2(int x , int y )
 	{
 	    switch( board[x][y] )
